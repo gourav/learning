13160aa7db33034c6c9d96eaf6e42a03fad2b9f0
Gaurav Tiwari <52605106+gourav@users.noreply.github.com>
2020-04-08 00:10:04 +0530

    add test to check if default class is acccessible to sub packages

---
+++ Java/OCJA/1.declaration-and-access-control/is_default_class_accessible_to_sub_packages/cars/audi/Audi.java
@@ -0,0 +1,13 @@
+package cars.audi;
+
+import cars.Car;
+
+/**
+ * I am proud implemenation of an Audi car.
+ */
+public class Audi extends Car {
+
+	public void printMe() {
+		System.out.println( "I am an Audi." );
+	}
+}
