cf6d8661ac5c74d5990f4e9971e9bb8ce6575881
Gaurav Tiwari <52605106+gourav@users.noreply.github.com>
2020-04-08 02:58:32 +0530

    add test to check if an interface can exist without initializing constant variable

---
+++ Java/OCJA/1.declaration-and-access-control/2.can_interface_constant_have_no_value/CanInterfaceConstantHaveNoValueTest.java
@@ -0,0 +1,8 @@
+/**
+ * This test is simply to verify if we can have an interface that though defines a constants but does not initializes it.
+ */
+public interface CanInterfaceConstantHaveNoValueTest {
+
+	int MAX_VALUE;
+
+} 
