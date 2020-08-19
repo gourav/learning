7153cb42502151a27802d824d094a0a43488c6a2
Gaurav Tiwari <52605106+gourav@users.noreply.github.com>
2020-08-19 21:55:42 +0530

    Add exmaple of using new line in printf statement

---
+++ Java/Early-Objects/001.Getting-Started/PrintFTutorial.java
@@ -0,0 +1,11 @@
+public class PrintFTutorial {
+
+    public static void main( String ... args ) {
+
+        /*
+         * Notice that to print new line, %n is used in place of \n in printf call.
+         */
+        System.out.printf( "%s%n%s", "Welcome to", "Java Programming." );
+
+    }
+}
