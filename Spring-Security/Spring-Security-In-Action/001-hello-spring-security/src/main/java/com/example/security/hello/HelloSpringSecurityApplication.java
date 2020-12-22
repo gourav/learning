a5b8a29ccf9aa054415011cc77bf4c9f4eef5467
Gaurav Tiwari <_gaurav@Gauravs-Mac-mini.local>
2020-12-22 10:09:33 +0530

    Adds example of spring security

---
+++ Spring-Security/Spring-Security-In-Action/001-hello-spring-security/src/main/java/com/example/security/hello/HelloSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringSecurityApplication.class, args);
+    }
+
+}
