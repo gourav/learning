0b9c5373ffe74417b7878f34de51c563b412924c
Gaurav Tiwari <52605106+gourav@users.noreply.github.com>
2021-06-06 14:31:07 +0530

    TDD - Root of all evil - Removed duplicated currency.

--- TDD/tdd-by-example/src/main/java/dev/inventio/domain/domain/Dollar.java
+++
@@ -1,9 +0,0 @@
-package dev.inventio.domain.domain;
-
-class Dollar extends Money {
-
-  Dollar(int amount, String currency) {
-    super(amount, currency);
-  }
-
-}
--- TDD/tdd-by-example/src/main/java/dev/inventio/domain/domain/Franc.java
+++
@@ -1,9 +0,0 @@
-package dev.inventio.domain.domain;
-
-class Franc extends Money {
-
-  Franc(int amount, String currency) {
-    super(amount, currency);
-  }
-
-}
